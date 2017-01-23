e76225700c1e6382f9e68b58ccf99ec8b676b948
sundial778 <amcclintock@atl.yearup.org>
2017-01-22 21:41:57 -0500

    updated js and index html
    bloppoownenw;e srvwerfqw

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
-// Client ID:
-// 8dfe31d8c7a44b1a953ba9e3f9b251fc
-// Client Secret
-// :
-// c04470cf1ab248ff88521866b35d2396
+// JavaScript source code
+$(document).ready(function () {
+    const url = 'https://api.spotify.com/v1';
 
-//Main class for all tracks
+    const Client_ID = '8dfe31d8c7a44b1a953ba9e3f9b251fc'
+   const Client_Secret=  'c04470cf1ab248ff88521866b35d2396'
 
-class Tracks{
+    function callMeBeepMe(que) {
+        //if you wanna reach me
+        $.ajax({
+            url: url + '/search/',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                q: que,
+                type: 'artist'
+            },
+            success: function (json) {
+                $('#resp').attr('src', './img/green_check.png');
+                // do stuff with json 
+                var stuff = []
+                
+                    
+                
+                
+                stuff.push('<li>' + json.artists.items[0].id + '</li>')
+                $('#values').append(stuff.join(''))
+                console.log(json)
+            },
+            error:function(){
+                $('#resp').attr('src','./img/red_x.png');
+            }      
+        });
+    }
+    var searchName;
+    
+    $('#submit').click(function (e) {
+        
+         searchName = $('#searchBox').val()
+         callMeBeepMe(searchName);
+            e.preventDefault();
+        });
+   
+    
 
-	constructor( artist, album, name, albumImage) {
-		this. artist =  artist;
-		this.album = album;
-		this.name = name;
-		this.albumImage = albumImage;
-		this.votes = 0
-	}
-
-	 upvote(){
-		this.votes = this.votes +=1
-	}
-	 downVote(){
-		return this.votes = this.votes -=1
-	}
-
-};
-
-let tempTrack= new Tracks("Coldplay","Sparks","Foo","Bar");
+});
